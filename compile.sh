73f7de9857d33191c5391576dee3d307aec02636
David Gomes <davidrafagomes@gmail.com>
2016-03-21 14:45:45 +0000

    Fixes compile.sh for good zipping

--- compile.sh
+++ compile.sh
@@ -3,4 +3,4 @@ lex mccompiler.l
 gcc -c ast.c -o ast
 gcc -o mccompiler ast mccompiler.tab.c lex.yy.c -ll -ly
 rm mccompiler.zip
-zip -r mccompiler.zip ./* > .zipsuccess
+zip mccompiler.zip mccompiler.l mccompiler.y ast.c ast.h
