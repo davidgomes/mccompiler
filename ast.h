80d83b7d89fc4f7fcecb687344db1c0575ac7f15
Marta Mercier Portugal M da Silva <MMercier@Martas-MBP.lan>
2016-03-25 23:50:02 +0000

    Fixes node types strings

--- ast.h
+++ ast.h
@@ -4,48 +4,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char* NODE_PROGRAM = "Program";
-char* NODE_DECLARATION = "Declaration";
-char* NODE_ARRAYDECLARATION = "ArrayDeclaration";
-char* NODE_FUNCDECLARATION = "FuncDeclaration";
-char* NODE_FUNC_EFINITION = "FuncDefinition";
-char* NODE_PARAMLIST = "ParamList";
-char* NODE_FUNCBODY = "FuncBody";
-char* NODE_PARAMDECLARATION = "ParamDeclaration";
-char* NODE_STATLIST = "StatList";
-char* NODE_IF = "If";
-char* NODE_FOR = "For";
-char* NODE_RETURN = "Return";
-char* NODE_OR = "Or";
-char* NODE_AND = "And";
-char* NODE_EQ = "Eq";
-char* NODE_NE = "Ne";
-char* NODE_LT = "Lt";
-char* NODE_GT = "Gt";
-char* NODE_LE = "Le";
-char* NODE_GE = "Ge";
-char* NODE_ADD = "Add"
-char* NODE_SUB = "Sub";
-char* NODE_MUL = "Mul";
-char* NODE_DIV = "Div";
-char* NODE_MOD = "Mod";
-char* NODE_NOT = "Not";
-char* NODE_MINUS = "Minus";
-char* NODE_PLUS = "Plus";
-char* NODE_ADDR = "Addr";
-char* NODE_DEREF = "Deref";
-char* NODE_STORE = "Store";
-char* NODE_COMMA = "Comma";
-char* NODE_CALL = "Call";
-char* NODE_CHAR = "Char";
-char* NODE_CHRLIT = "Chrlit";
-char* NODE_ID = "Id";
-char* NODE_INT = "Int";
-char* NODE_INTLIT = "Intlit";
-char* NODE_POINTER = "Pointer";
-char* NODE_STRLIT = "Strlit";
-char* NODE_VOID = "Void";
-char* NODE_NULL = "Null";
+char* node_types[] = {
+  "Program",
+  "Declaration",
+  "ArrayDeclaration",
+  "FuncDeclaration",
+  "FuncDefinition",
+  "ParamList",
+  "FuncBody",
+  "ParamDeclaration",
+  "StatList",
+  "If",
+  "For",
+  "Return",
+  "Or",
+  "And",
+  "Eq",
+  "Ne",
+  "Lt",
+  "Gt",
+  "Le",
+  "Ge",
+  "Add",
+  "Sub",
+  "Mul",
+  "Div",
+  "Mod",
+  "Not",
+  "Minus",
+  "Plus",
+  "Addr",
+  "Deref",
+  "Store",
+  "Comma",
+  "Call",
+  "Char",
+  "Chrlit",
+  "Id",
+  "Int",
+  "Intlit",
+  "Pointer",
+  "Strlit",
+  "Void",
+  "Null"
+};
+
 
 typedef enum { NODE_PROGRAM, NODE_DECLARATION, NODE_ARRAYDECLARATION, NODE_FUNCDECLARATION,
   NODE_FUNC_EFINITION, NODE_PARAMLIST, NODE_FUNCBODY, NODE_PARAMDECLARATION, NODE_STATLIST, NODE_IF, NODE_FOR, NODE_RETURN, NODE_OR,
