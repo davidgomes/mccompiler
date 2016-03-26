cd555d5175497cfb2c29ea3f85602db979f5aa8d
Marta Mercier Portugal M da Silva <MMercier@Martas-MBP.lan>
2016-03-26 12:59:29 +0000

    Completes ast_insert_node function

--- ast.c
+++ ast.c
@@ -1,4 +1,8 @@
 #include "ast.h"
+#include <stdarg.h>
+#include <string.h>
+
+node_t *ast, *merge_nodes[2048];
 
 char* node_types[] = {
   "Program",
@@ -47,8 +51,34 @@ char* node_types[] = {
 
 node_t* ast_insert_node(nodetype_t nodetype, int to_use, int node_operands, ...){
   node_t *new_node, **tmp;
+  int i, nodes = 0;
   va_list args;
 
+  new_node = ast_create_node(nodetype);
+
+  tmp = merge_nodes;
+  va_start(args, node_operands);
+
+  while(node_operands--){
+    node_t *t = va_arg(args, node_t *);
+
+    if(t == NULL)
+      continue;
+    else if(!t->to_use)
+      for(nodes += t->n_childs, i = 0; i < t->n_childs; i++)
+        *tmp++ = t->childs[i];
+    else{
+      *tmp++ = t;
+      nodes++;
+    }
+
+  }
+
+  new_node->childs = (node_t **) malloc (nodes * sizeof(node_t *));
+  memcpy(new_node->childs, merge_nodes, nodes * sizeof(node_t *));
+  new_node->n_childs = nodes;
+
+  va_end(args);
   return new_node;
 }
 
