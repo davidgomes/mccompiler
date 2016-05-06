25a740a2d31393d81b64cdeeb23895874200af4f
David Gomes <davidrafagomes@gmail.com>
2016-05-07 00:26:51 +0100

    adds some missing files

---
+++ code_gen.c
@@ -0,0 +1,5 @@
+#include "code_gen.h"
+
+void code_gen(node_t *which) {
+  printf("hello\n");
+}
---
+++ code_gen.h
@@ -0,0 +1,10 @@
+#ifndef CODE_GEN_H
+#define CODE_GEN_H
+
+#include <stdio.h>
+
+#include "structures.h"
+
+void code_gen(node_t *which);
+
+#endif
